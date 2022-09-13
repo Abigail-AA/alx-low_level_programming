fe33cea0751c41e6d34071bda5cdc8bdad877126
Abigail <akinbamireabigail@gmail.com>
2022-09-13 15:57:31 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print-alphabet_x10 - Print the alphabets in lower case 10 times
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 97; b < 123; b++)
+			_putchar(10);
+	}
+}
