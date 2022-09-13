e9a5ada0a4275dbc18294416bf9a700c951d3620
Abigail <akinbamireabigail@gmail.com>
2022-09-13 15:34:47 -0700

    cisfun

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print-alphabet - Alphabet game
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+		_putchar(x);
+
+	_putchar(10);
+}
