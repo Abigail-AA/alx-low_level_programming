d08e7f2d7cf3872a723208f4f4ad59d5d0ec5ab4
Abigail <akinbamireabigail@gmail.com>
2022-09-16 03:16:01 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers, from 0 to 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+	}
+
+	_putchar(10);
+}
