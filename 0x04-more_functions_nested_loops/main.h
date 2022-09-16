3bf8d940dac16ef0167c5da428d7b461052d23d5
Abigail <akinbamireabigail@gmail.com>
2022-09-16 10:21:13 -0700

    numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,9 +2,9 @@
 #define _MAIN_H_
 
 int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
 int _putchar(char c);
+int _isdigit(int c);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
