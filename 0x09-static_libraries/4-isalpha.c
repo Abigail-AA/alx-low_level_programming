0aad8a17b1a7174755893c756e2f02ad7a30fed5
Abigail <akinbamireabigail@gmail.com>
2022-09-30 20:30:56 -0700

    not everything that looks easy is easy

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if character is an alphabet
+ * @c: character
+ *
+ * Return: 1 if true, if false
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
